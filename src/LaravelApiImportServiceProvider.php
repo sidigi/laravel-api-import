87a57eaf8c3088a6524b1459b07f9313d552743f
Sidigi <sidigicoder@gmail.com>
2019-05-24 23:46:56 +0500

    add laravel stuff

---
+++ src/LaravelApiImportServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace sidigi\LaravelApiImport;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelApiImportServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()){
+            $this->registerPublishing();
+        }
+    }
+
+    public function register()
+    {
+
+    }
+
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/laravel-api-import.php' => config_path('laravel-api-import.php')
+        ], 'laravel-api-import-config');
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace sidigi\LaravelApiImport\Tests;
+
+use sidigi\LaravelApiImport\LaravelApiImportServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LaravelApiImportServiceProvider::class
+        ];
+    }
+}
